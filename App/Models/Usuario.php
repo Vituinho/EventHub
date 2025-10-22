32fededf98f27e4d64e1979213d44a47c6c87311
Vituinho <trabalhovictor531@gmail.com>
2025-10-22 10:22:23 -0300

    Feat: Adicionando a Função de Autenticar, que faz uma verificação da senha, e transforma em password_hash, pra mais segurança.

--- App/Models/Usuario.php
+++ App/Models/Usuario.php
@@ -56,11 +56,29 @@ class Usuario extends Model {
         $stmt = $this->db->prepare($query);
         $stmt->bindValue(':nome', $this->__get('nome'));
         $stmt->bindValue(':email', $this->__get('email'));
-        $stmt->bindValue(':senha', $this->__get('senha'));
+        $stmt->bindValue(':senha', password_hash($this->__get('senha'), PASSWORD_DEFAULT));
         $stmt->execute();
 
     return $this;
-}
+    }
+
+    public function autenticar() {
+        $query = "SELECT id, nome, email, senha FROM usuarios WHERE email = :email";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':email', $this->__get('email'));
+        $stmt->execute();
+
+        $usuario = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+        if ($usuario && password_verify($this->__get('senha'), $usuario['senha'])) {
+            $this->__set('id', $usuario['id']);
+            $this->__set('nome', $usuario['nome']);
+            $this->__set('email', $usuario['email']);
+            return $this; // retorna o usuário autenticado
+        } else {
+            return false;
+        }
+    }
 
 
 }
