320f28bf1333bb887ff594efddfb8de1c04ac4cb
Vituinho <trabalhovictor531@gmail.com>
2025-10-24 11:04:25 -0300

    Feat: Adicionando Função de Cadastrar Eventos no controller de eventos.

--- App/Controllers/EventosController.php
+++ App/Controllers/EventosController.php
@@ -6,12 +6,34 @@ namespace App\Controllers;
 use MF\Controller\Action;
 use MF\Model\Container;
 
+session_start();
+
 class EventosController extends Action {
 
     public function CadastroEventos() {
 		$cadastro_eventos = Container::getModel('Eventos');
-		$this->view->eventos = $cadastro_eventos->salvar();
+		$this->view->eventos = $cadastro_eventos->getAll();
 		$this->render('cadastro_eventos');
+	}
+
+	public function CadastrarEventos() {
+
+		$cadastro_eventos = Container::getModel('Eventos');
+
+		// Setando valores
+		$cadastro_eventos->__set('nome', $_POST['nome'] ?? '');
+		$cadastro_eventos->__set('data', $_POST['data'] ?? '');
+		$cadastro_eventos->__set('local', $_POST['local'] ?? '');
+		$cadastro_eventos->__set('detalhes', $_POST['detalhes'] ?? '');
+		$cadastro_eventos->__set('id_usuario', $_SESSION['id_usuario']); 
+
+		$this->view->eventos = $cadastro_eventos->salvar();
+
+		$this->view->eventos = $cadastro_eventos->getAll();
+		$this->render('cadastro_eventos');
+
+		header('Location: /home');
+		exit;
 	}
 
 }
